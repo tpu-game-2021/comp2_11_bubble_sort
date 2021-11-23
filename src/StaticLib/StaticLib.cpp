81b3cfdab3e052b3ea04b98f2df95c6a0a4d196f
g2127043@st.t-kougei.ac.jp <g2127043@st.t-kougei.ac.jp>
2021-11-23 11:37:48 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,22 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin ==NULL) return false;
+	if (begin > end) return false;
+	int i, j;
+	item tmp;
+	for (i = 0; begin + i < end; i++)
+	{
+		if (begin[i].value[strlen(begin[i].value) + 1] != begin[i].value[sizeof(begin[i].value) - 1]) return false;
+		for (j = i + 1; begin + j < end; j++)
+		{
+			if (begin[i].key > begin[j].key)
+			{
+				tmp = begin[i];
+				begin[i] = begin[j];
+				begin[j] = tmp;
+			}
+		}
+		if(begin + j > end) return false;
+	}
 }

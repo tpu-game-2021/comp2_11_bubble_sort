e3d8c15c08fe03c40e6117c32b21212a5f235352
GTM106 <raito125raito@gmail.com>
2021-11-25 23:30:09 +0900

    基本のテストは通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,21 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL || end == NULL)return false;
+	if (end < begin)return false;
+	int num = end - begin;
+	if (begin + num != end)return false;
+	
+	for (item* p = begin; p < end; p++) {
+		for (item* p = begin; p < end-1; p++) {
+			if (p->key > (p + 1)->key)
+			{
+				item temp = *p;
+				*p = *(p + 1);
+				*(p + 1) = temp;
+			}
+		}
+	}
+	return true;
+	
 }

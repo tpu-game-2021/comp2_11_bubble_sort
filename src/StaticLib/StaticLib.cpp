5dd760d843fef48c29c37a88cf3a14926a588c62
matsuurataketo <g2127063@st.t-kougei.ac.jp>
2021-11-28 21:23:20 +0900

    一部テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,52 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL)
+	{
+		return false;
+	}
+
+	if (end == NULL)
+	{
+		return false;
+	}
+	
+	if (begin > end)
+	{
+		return false;
+	}
+	if (begin == end)
+	{
+		return false;
+	}
+
+	int i, j;
+	item ChangeBox;
+	for (i = 0; begin + i < end; i++)
+	{
+		for (j = i + 1; begin + j < end; j++)
+		{
+			if (begin[i].key > begin[j].key)
+			{
+
+				ChangeBox = begin[i];
+				begin[i] = begin[j];
+				begin[j] = ChangeBox;
+
+			}
+		}
+		if (begin + j > end)
+		{
+			return false;
+		}
+		if (begin > end)
+		{
+			return false;
+		}
+		if (begin == end)
+		{
+			return false;
+		}
+		
+	}
 }

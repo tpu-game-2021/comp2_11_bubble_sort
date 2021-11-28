0a9965088d7ceabcf7b46b5bfddafb7e33d5bd7f
SugarWind <g2127037@st.t-kougei.ac.jp>
2021-11-28 21:25:59 +0900

    半端な～を除くテストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,27 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
+	if(begin == NULL|| end == NULL)
 	return false;
+	
+	if (end < begin)
+		return false;
+
+	int a, z;
+	int tnum = end - begin;
+	item tmp;
+
+	for (a = 0; a < tnum; a++)
+	{
+		for (z = a + 1; z < tnum; z++)
+		{
+			if (begin[a].key > begin[z].key)
+			{
+				tmp = begin[a];
+				begin[a] = begin[z];
+				begin[z] = tmp;
+			}
+		}
+	}
+	return true;
 }

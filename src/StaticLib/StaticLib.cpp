1dadb1e273100f7c33871613fa97c4f457082abe
ryouta <ryouta32ryouta@gmail.com>
2021-11-28 23:32:27 +0900

    変更

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,24 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL || end == NULL) {
+		return false;
+	}
+
+	if (end < begin) {
+		return false;
+	}
+
+	for (int i = 0; i < end - begin; i++) {
+
+		for (int j = i + 1; j < end - begin; j++) {
+			if (begin[i].key > begin[j].key) {
+
+				item temp = begin[i];
+				begin[i] = begin[j];
+				begin[j] = temp;
+			}
+		}
+	}
+	return true;
 }

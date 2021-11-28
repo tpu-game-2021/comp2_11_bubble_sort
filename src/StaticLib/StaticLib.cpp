87fa52f53b56c33c7fd7ea67a04c06c0165bf8cc
umee113 <mayu2943@icloud.com>
2021-11-29 00:49:02 +0900

    テストを通せませんでした。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,24 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
+	int i, j,tmp;
+
+	//key=int型　value=cher型　
+	//begin,end=item 型？ const
+
+	for (i = 0; i < end; i++)
+	{
+		for (j =end + 1; j <end; j++)
+		{
+			if (begin.key[i] < begin.key[j])
+			{
+				tmp = begin[i];
+				begin[i] = begin[j];
+				begin[j] = tmp;
+
+			}
+		}
+	}
+
 	return false;
 }

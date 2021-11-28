8323717590035a23719e0ce1a7871462d2d46421
zakiAKR <zakican.ak@gmail.com>
2021-11-28 23:19:53 +0900

    テストコードを通せませんでした。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,44 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (begin == NULL || end == NULL) return false;
+	//if (begin > end)return false;
+	int n = (end - begin) / sizeof(item);
+
+	if (begin->key > end->key)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = i + 1; j < n; j++)
+			{
+				if (begin[i].key > begin[j].key)
+				{
+					int tnp = begin[i].key;
+					begin[i].key = begin[j].key;
+					begin[j].key = tnp;
+				}
+			}
+		}
+		return true;
+	}
+	else if (begin < end) return false;
+	else return false;
+
+	/*if (begin->key < end->key)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = i + 1; j < n; j++)
+			{
+				if (begin[i].key > begin[j].key)
+				{
+					int tnp = begin[i].key;
+					begin[i].key = begin[j].key;
+					begin[j].key = tnp;
+				}
+			}
+		}
+		return true;
+	}*/
+	//else return false;
 }
